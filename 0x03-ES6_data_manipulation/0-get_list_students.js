3356b29e5dd912f25987e19aff6f6eadcd261af4
Damaris Muange <mdamarisnduku@gmail.com>
2024-10-08 23:17:40 +0300

    Add files via upload

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,17 +1,15 @@
-#!/usr/bin/env bash
 export default function getListStudents() {
-	return [{
-id: 1,
-	    firstName: 'Guillaume',
-	    location: 'San Francisco',
-	}, {
-id: 2,
-	    firstName: 'James',
-	    location: 'Columbia',
-	}, {
-id: 5,
-	    firstName: 'Serena',
-	    location: 'San Francisco',
-	}];
+  return [{
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  }, {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  }, {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  }];
 }
-
